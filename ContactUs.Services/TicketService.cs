c63809c6693b628528786de954f4d6ad20574f84
pongpob <pongpob@aerothai.co.th>
2015-09-17 14:04:14 +0700

    User can submit a new ticket
    Resolved #2

--- ContactUs.Services/TicketService.cs
+++ ContactUs.Services/TicketService.cs
@@ -12,20 +12,33 @@ namespace ContactUs.Services
     {
         public override IRepository<Ticket> Repository
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get;
+            set;
         }
 
         public override Ticket Find(params object[] keys)
         {
             string id = (string)keys[0];
             return Query(t => t.Id == id).SingleOrDefault();
+        }
+        public override Ticket Add(Ticket item)
+        {
+            //item.Id = "0123ab";
+            item.Id = generateTicketId();
+            item.LastActivityDate = SystemTime.Now();
+            return base.Add(item);
+        }
+        private string generateTicketId()
+        {
+            var r = new Random();
+            var chars = "0123456789abcdef";
+            var sb = new StringBuilder(6);
+
+            for (int i = 0; i < 6; i++)
+            {
+                sb.Append(chars[r.Next(chars.Length)]);
+            }
+            return sb.ToString();
         }
     }
 }
